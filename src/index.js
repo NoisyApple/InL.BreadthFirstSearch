717b58db3eeb16f2a36397c04340c6d6fb313991
NoisyApple <leonel.aguirre77@gmail.com>
2020-10-01 00:57:08 -0500

    Zoom and displacement implemented.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles.scss";
+require("./styles.scss");
 import p5 from "p5";
 import Graph from "./Graph";
 import Node from "./Node";
@@ -11,12 +11,26 @@ const springLength = 5;
 const r = 4;
 const forceReduction = 0.98;
 
-let scale = 1;
+const controls = {
+  viewZoom: {
+    zoom: 1,
+    x: 0,
+    y: 0,
+    minZoom: 0.5,
+    maxZoom: 4,
+  },
+  viewPosition: {
+    prevX: null,
+    prevY: null,
+    isDragging: false,
+  },
+};
 
 let graph;
 
 window.onload = () => {
   const sketch = (p5) => {
+    // SETUP +++
     p5.setup = () => {
       let canvas = p5.createCanvas(width, height);
       canvas.parent("Canvas");
@@ -42,17 +56,75 @@ window.onload = () => {
         }
       }
     };
+    // SETUP ---
 
+    // DRAW LOOP +++
     p5.draw = () => {
-      p5.scale(scale);
       p5.background("#adb");
+
+      p5.text(`zoom: ${controls.viewZoom.zoom.toFixed(2)}`, 10, 20);
+
+      p5.translate(controls.viewZoom.x, controls.viewZoom.y);
+      p5.scale(controls.viewZoom.zoom);
+
       graph.draw();
       graph.update();
     };
+    // DRAW LOOP ---
 
-    // p5.mouseWheel = (e) => {
-    //   scale += (e.delta / 1000) * -1;
-    // };
+    // ZOOM HANDLER +++
+    p5.mouseWheel = (e) => {
+      const { x, y, delta } = e;
+      const wheelFactor = 0.1;
+      const zoom = wheelFactor * (delta / -100);
+
+      const xWeight =
+        (x - controls.viewZoom.x) / (width * controls.viewZoom.zoom);
+      const yWeight =
+        (y - controls.viewZoom.y) / (height * controls.viewZoom.zoom);
+
+      if (
+        controls.viewZoom.zoom > controls.viewZoom.minZoom &&
+        controls.viewZoom.zoom < controls.viewZoom.maxZoom
+      ) {
+        controls.viewZoom.x -= xWeight * width * zoom;
+        controls.viewZoom.y -= yWeight * height * zoom;
+      }
+      controls.viewZoom.zoom += zoom;
+
+      controls.viewZoom.zoom = p5.constrain(
+        controls.viewZoom.zoom,
+        controls.viewZoom.minZoom,
+        controls.viewZoom.maxZoom
+      );
+    };
+    // ZOOM HANDLER ---
+
+    // MOTION HANDLERS +++
+    p5.mousePressed = () => {
+      controls.viewPosition.isDragging = true;
+      controls.viewPosition.prevX = p5.mouseX;
+      controls.viewPosition.prevY = p5.mouseY;
+    };
+
+    p5.mouseReleased = () => {
+      controls.viewPosition.isDragging = false;
+      controls.viewPosition.prevX = null;
+      controls.viewPosition.prevY = null;
+    };
+
+    p5.mouseDragged = () => {
+      if (controls.viewPosition.isDragging) {
+        let difX = p5.mouseX - controls.viewPosition.prevX;
+        let difY = p5.mouseY - controls.viewPosition.prevY;
+
+        controls.viewZoom.x += difX;
+        controls.viewZoom.y += difY;
+        controls.viewPosition.prevX = p5.mouseX;
+        controls.viewPosition.prevY = p5.mouseY;
+      }
+    };
+    // MOTION HANDLERS ---
   };
 
   new p5(sketch);
